123ec7012778d1590b8baebd3c1e54349f88898e
hoangminh5210119 <dohoangminh006@gmail.com>
2020-03-18 19:10:52 +0700

    add sort file name

--- getXBM2File.py
+++ getXBM2File.py
@@ -7,11 +7,15 @@ def main(path):
 
 
 def copyMultiFile2aFile(path):
-    print(path)
     os.chdir(path + "/image")
     _data_ = '#include "Arduino.h"\n\n\n'
+    filenames = []
     for file in glob.glob("*.xbm"):
-        f = open(file, "r")
+        filenames.append(file)
+    filenames.sort()
+    # print(filenames)
+    for filename in filenames:
+        f = open(filename, "r")
         data = f.read()
         data = data.replace("static char", "const char")
         data = data.replace("=", "PROGMEM = ")
@@ -26,14 +30,20 @@ def multiFile2aVariable(path):
     os.chdir(path + "/image")
     size = 0
     _data_file_ = ""
+    filenames = []
     for file in glob.glob("*.xbm"):
-        # print(file)
-        f = open(file, "r")
+        filenames.append(file)
+
+    filenames.sort()
+
+    for filename in filenames:
+        f = open(filename, "r")
         data = f.read()
         a, b = str(data).split("=")
         c = b.replace(";", "")
         _data_file_ += c + ",\n"
         size = c.count("0x")
+
     datafile = '#include "Arduino.h"\n\n\n'
     datafile += "#define image_width 32\n"
     datafile += "#define image_height 32\n\n\n"
